d940d666bcae115a4c83830168b864404aa8ade6
Ziga Sinko <ziga.sinko@genialis.com>
2020-04-06 15:36:33 +0200

    Add create-react-app code with configured prettier

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import './App.css';
+
+function App(): ReactElement {
+    return (
+        <div className="app">
+            <header className="App-header">
+                <h1>dictyExpress</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
