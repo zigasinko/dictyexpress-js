ccaae5c4694a9484b89f563edccafe8d16227faa
Jan Kumer <janybravo@gmail.com>
2021-02-26 13:57:51 +0100

    Add fixed WSS API endpoint config for test environment

--- src/api/base.ts
+++ src/api/base.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 export const baseUrl = location.origin;
 export const apiUrl = `${baseUrl}/api`;
 export const authApiUrl = `${baseUrl}/rest-auth`;
-export const webSocketUrl = `${baseUrl}/ws`;
-
+export const webSocketUrl =
+    process.env.REACT_APP_TEST_WSS ??
+    // eslint-disable-next-line no-restricted-globals
+    `ws${location.hostname === 'localhost' ? '' : 's'}://${location.host}/ws/`;
 export const sessionId = uuidv4();
