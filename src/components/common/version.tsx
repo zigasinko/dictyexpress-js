7c31823cb1148cd437f3a72d55ac6a271f04b12f
Jan Kumer <janybravo@gmail.com>
2021-03-02 14:09:41 +0100

    Fix SHA string in version output

--- src/components/common/version.tsx
+++ src/components/common/version.tsx
@@ -4,7 +4,7 @@ export const Version = (): ReactElement => (
     <span className="version">
         {process.env.REACT_APP_VERSION}
         {process.env.REACT_APP_DEPLOY_ENV === 'staging'
-            ? `+${(process.env.REACT_APP_COMMIT_SHA as string).substring(8)}`
+            ? `+${(process.env.REACT_APP_COMMIT_SHA as string).substring(0, 8)}`
             : ''}
     </span>
 );
