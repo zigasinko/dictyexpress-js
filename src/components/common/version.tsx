1f79ea02aae57d1d697b11e728c769c8a341e035
zigasinko <ziga.sinko@genialis.com>
2024-08-16 10:10:54 +0200

    Fix build GH action

--- src/components/common/version.tsx
+++ src/components/common/version.tsx
@@ -1,10 +1,5 @@
 import React, { ReactElement } from 'react';
 
 export const Version = (): ReactElement => (
-    <span className="version">
-        {import.meta.env.VITE_APP_VERSION}
-        {import.meta.env.VITE_APP_DEPLOY_ENV === 'staging'
-            ? `+${(import.meta.env.VITE_APP_COMMIT_SHA as string).substring(0, 8)}`
-            : ''}
-    </span>
+    <span className="version">{import.meta.env.VITE_APP_VERSION}</span>
 );
