f091cd8821a9910f024688877838e7f2803f252c
zigasinko <ziga.sinko@genialis.com>
2024-08-16 10:10:34 +0200

    Add new references

--- src/components/landing/references/references.tsx
+++ src/components/landing/references/references.tsx
@@ -10,6 +10,23 @@ const References = (): ReactElement => (
         <Title>References</Title>
         <ReferencesContainer>
             <Reference
+                authors="Katoh-Kurasawa M, Hrovatin K, Hirose S, Webb A, Ho H, Zupan B, Shaulsky G"
+                source="Genome Research 2021"
+            >
+                <a href="https://pubmed.ncbi.nlm.nih.gov/34183452/">
+                    Transcriptional milestones in <i>Dictyostelium</i> development
+                </a>
+            </Reference>
+            <Reference
+                authors="Swatson WS, Katoh-Kurasawa M, Shaulsky G, Alexander S"
+                source="PLoS One 2017"
+            >
+                <a href="https://preview.ncbi.nlm.nih.gov/pmc/articles/PMC5685611/">
+                    Curcumin affects gene expression and reactive oxygen species via a PKA dependent
+                    mechanism in <i>Dictyostelium discoideum</i>
+                </a>
+            </Reference>
+            <Reference
                 authors="Rosengarten RD, Santhanam B, Kokosar J, Shaulsky G"
                 source="Genes|Genomes|Genetics (G3) 2017"
             >
@@ -19,12 +36,40 @@ const References = (): ReactElement => (
                 </a>
             </Reference>
             <Reference
+                authors="Katoh-Kurasawa M, Santhanam B, Shaulsky G"
+                source="Journal of Cell Science 2016"
+            >
+                <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4852770/">
+                    The GATA transcription factor gene gtaG is required for terminal differentiation
+                    in <i>Dictyostelium</i>
+                </a>
+            </Reference>
+            <Reference
                 authors="Chen X, Köllner TG, Jia Q, Norris A, Santhanam B, Rabe P, Dickschat JS, Shaulsky G, Gershenzon J, Chen F"
                 source="PNAS 2016"
             >
                 <a href="https://www.ncbi.nlm.nih.gov/pubmed/27790999">
                     Terpene synthase genes in eukaryotes beyond <br />
                     plants and fungi: Occurrence in social amoebae
+                </a>
+            </Reference>
+            <Reference
+                authors="Rosengarten RD, Santhanam B, Fuller D, Katoh-Kurasawa M, Loomis WF, Zupan B, Shaulsky G"
+                source="BMC Genomics 2015"
+            >
+                <a href="https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-015-1491-7">
+                    Leaps and lulls in the developmental transcriptome of{' '}
+                    <i>Dictyostelium discoideum</i>
+                </a>
+            </Reference>
+            <Reference
+                authors="Hirose S, Santhanam B, Katoh-Kurasawa M, Shaulsky G, Kuspa A"
+                source="Development 2015"
+            >
+                <a href="https://pubmed.ncbi.nlm.nih.gov/26395484/">
+                    Allorecognition, via TgrB1 and TgrC1, mediates the transition from
+                    unicellularity to multicellularity in the social amoeba{' '}
+                    <i>Dictyostelium discoideum</i>
                 </a>
             </Reference>
             <Reference
